a0194027204d47fcb6742286001a4533f5b5049a
Ishu <artik11522@gmail.com>
2025-11-08 23:27:40 +0530

    updated the dockers and added the .gitignore of the roor

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(generalLimiter); // Redis-backed rate limiter
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:5173', //will change
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true
 }))
 
